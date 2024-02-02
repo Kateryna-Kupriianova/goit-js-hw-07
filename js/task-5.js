bbc76281c36b42aaedeaa55ba8eb3779be94ed0b
Kateryna-Kupriianova <kupriyanov.forever@gmail.com>
2024-02-02 23:07:42 +0200

    update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const widget = document.querySelector('.widget');
+const colorSpan = widget.querySelector('.color');
+changeColorBtn.addEventListener("click", function () {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+})
+
+
+
